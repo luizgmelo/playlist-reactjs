3cf415a243336c0f31d4ef9c71628146b839bdbc
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2025-01-23 20:37:18 -0300

    feat: add pokemons in dashboard component

--- project-03/src/Dashboard/index.tsx
+++ project-03/src/Dashboard/index.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 import styles from './styles.module.scss';
+import { PokemonType } from "../types/PokemonType";
 
 export default function Dashboard() {
+
+  const [pokemons, setPokemons] = useState<PokemonType[]>([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await fetch("http://localhost:3000/pokemon");
+
+      const data = await response.json();
+
+      setPokemons(data);
+    }
+
+    loadData()
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1>Dashboard</h1>
+
+      <ul className={styles["container-pokemons"]}>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <strong>{pokemon.type}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
